d4114559c5bd9677ab2f28b158a5ea885e071926
白羽 <50762038+1665169869@users.noreply.github.com>
2022-11-28 13:24:55 +0800

    Update 国开刷视频.js

--- 国开刷视频.js
+++ 国开刷视频.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         国开自动播放下一集和解除切屏暂停视频限制
 // @namespace    http://index.xn--90wo2bk78a.love/
-// @version      1.1.0
+// @version      1.1.2
 // @description  国开自动播放下一集和解除切屏暂停视频限制 目前测试中
 // @author       白羽
 // @include      *://lms.ouchn.cn/course/*
@@ -26,13 +26,17 @@ function getCookie(cname) {
     return "";
 }
 
-function gk_pageback() { // 返回课程列表
-    document.querySelector("body > div.wrapper > div.main-content.gtm-category > div:nth-child(9) > div > div.activity-area.clearfix.online-video-area > div.module-syllabus-nav.sidebar > div.return-link > a").click();
+function gk_pageback(time) { // 返回课程列表
+    setTimeout(function () {
+        document.querySelector(".return-link > a").click();
+    }, time ? time : 2000)
 }
+
 
 function onVideo() {
     let myval = setInterval(function () { // 设置个定时任务 判断视频是否加载出来
         let video = document.getElementsByTagName("video")[0];
+        let audio = document.getElementsByTagName("audeo")[0];
         if (video !== undefined) {
             if (parseInt(video.duration) === parseInt(video.currentTime) ||
                 document.getElementsByClassName("mvp-time-display")[0].getElementsByTagName("span")[0].textContent
@@ -49,6 +53,12 @@ function onVideo() {
                 gk_pageback();
             }
             clearInterval(myval);
+        } else if (audio !== undefined) {
+            if (parseInt(audio.duration) === parseInt(audio.currentTime) || document.querySelector("span.current.ng-binding").textContent === document.querySelector("div.duration span.ng-binding").textContent) {
+                audio.currentTime = 0;
+            }
+            setTimeout(function () { audio.volume = 0; document.querySelector(".audio-player-wrapper > div > a").click() }, 3000);
+            audio.onpause
         }
     }, 2000);
 }
@@ -65,6 +75,9 @@ document.onreadystatechange = function () {
                 let alllearning = $(".learning-activity");
                 for (let i = 0; i < alllearning.length; i++) {
                     let str = $(".learning-activity:eq(" + i + ") div.activity-operations-container .completeness").attr("tipsy-literal");
+                    if (str === undefined) {
+                        continue;
+                    }
                     let zf = str.match(/^<b>(\W+)<\/b>/)[1];
                     let type = str.match(/^<b>\W+<\/b><\/br>(\W+)/)[1];
                     let typeEum = -1;
@@ -78,16 +91,16 @@ document.onreadystatechange = function () {
                         typeEum = 4;
                     } else if (type.indexOf("完成指标：观看或下载所有参考资料附件") > -1) {
                         typeEum = 5;
+                    } else if (type === "完成指标：无") {
+                        typeEum = 6;
                     }
                     if (zf !== "已完成" && typeEum != -1) {
                         console.log("发现未完成的课程~");
                         document.cookie = "typeEum=" + typeEum;
-                        switch (typeEum) {
-                            case 2:
-                                $(".learning-activity:eq(" + i + ")>div").click();
-                                break;
+                        if (typeEum === 2 || typeEum === 4) {
+                            $(".learning-activity:eq(" + i + ")>div").click();
+                            break;
                         }
-                        break;
                     }
                 }
             }, 5000);
@@ -95,6 +108,22 @@ document.onreadystatechange = function () {
             switch (Number(getCookie("typeEum"))) {
                 case 2:
                     onVideo();
+                    break;
+                case 4:
+                    var timeId;
+                    timeId = setInterval(function () {
+                        let str = document.querySelector(".embeded-new-topic>i");
+                        if (str !== undefined && str !== null) {
+                            str.click();
+                            $("#add-topic-popup > div > div.topic-form-section.main-area > form > div:nth-child(1) > div.field > input").val("好好学习").trigger('change');
+                            setTimeout(function () {
+                                document.querySelector("#add-topic-popup > div > div.popup-footer > div > button.button.button-green.medium").click()
+                                gk_pageback(5000);
+                            }, 1000);
+                            clearInterval(timeId);
+                        }
+                    }, 4000);
+
                     break;
             }
         }
